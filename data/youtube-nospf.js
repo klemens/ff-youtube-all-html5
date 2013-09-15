1dda1d2fd1e7656c88c397be1d99859e4ed76cef
Klemens Schölhorn <klemens@schoelhorn.eu>
2013-09-15 04:54:54 +0200

    Improve spf detection and handling
    Spf is not touched anymore when using ie emulation because it works just fine there.
    Also using a more general approach which works without changes after the page has
    been changed with spf by looking at all click events and determining if they
    came from a link element.

--- data/youtube-nospf.js
+++ data/youtube-nospf.js
@@ -1,14 +1,23 @@
 // inspired by Maciej Katafiasz (http://mathrick.org)
 // http://userscripts.org/scripts/review/170194
 
-// disable spf links on non-video sites
-if(!(/\/watch.*/.test(window.location.pathname))) {
-    var stopPropagation = function(event) {
-        event.stopPropagation();
-    }
-    
-    var spfLinks = document.querySelectorAll("a.spf-link");
-    for(var i = 0; i < spfLinks.length; ++i) {
-        spfLinks[i].addEventListener("click", stopPropagation, true);
-    }
+// disable spf links on non-video sites when not using ie emulation
+if("ie" != self.options.settings["loadtype"] && window.wrappedJSObject.ytspf &&
+   window.wrappedJSObject.ytspf.enabled) {
+    window.addEventListener("click", function(event) {
+        if(/\/watch.*/.test(window.location.pathname)) {
+            return;
+        }
+
+        // check if click came from a link
+        var target = event.target;
+        while(target) {
+            if(target instanceof HTMLAnchorElement &&
+               target.classList.contains("spf-link")) {
+                event.stopPropagation();
+                return;
+            }
+            target = target.parentNode;
+        }
+    }, true);
 }
